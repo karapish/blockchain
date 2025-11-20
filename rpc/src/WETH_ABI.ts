0e4d4ee3678414fc3c2e3fa0836b770ad2bacb9e
Gleb Karapish <gleb@altarey.com>
2025-11-19 22:27:41 -0500

    Extend WETH ABI with `decimals` and `symbol` support.

--- rpc/src/WETH_ABI.ts
+++ rpc/src/WETH_ABI.ts
@@ -3,6 +3,8 @@ export const WETH_ABI = [
   'function withdraw(uint256 amount)',
   'function balanceOf(address owner) view returns (uint256)',
   'function approve(address spender, uint256 amount) returns (bool)',
+  'function decimals() view returns (uint8)',
+  'function symbol() view returns (string)',
   'event Transfer(address indexed from, address indexed to, uint256 value)',
   'event Approval(address indexed owner, address indexed spender, uint256 value)',
   'event Deposit(address indexed dst, uint256 wad)',
